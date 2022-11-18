d104b2f732c33e21cea64866677860d939dcbab2
INHO SHIN <72684256+42inshin@users.noreply.github.com>
2022-11-18 03:59:57 +0000

    ADD test.py: pylint

---
+++ test.py
@@ -0,0 +1,11 @@
+"""
+this is moudule docstring test file
+안적으면 pylint에서 docstring을 적어라고 계속 오류를 띄운다.
+"""
+def hello_word():
+    '''
+    function docstring comments
+    '''
+    return "hello"
+
+print(hello_word())
